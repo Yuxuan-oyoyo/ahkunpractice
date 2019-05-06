fec938b95db257399aab17a8b34fad277bbb5efb
kun <khuang002@gmail.com>
2019-05-06 23:41:38 +0800

    first program

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,6 +1,9 @@
 public class app {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        String greeting = "Welcome to Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
     }
 }
